a1f0bd0ba54870ab66854d00d80b9b3705930a6c
Ornã Keivison Fernandes Câmara <132477807+OrnanKeivison@users.noreply.github.com>
2024-11-01 07:19:50 -0300

    aula1

--- app.py
+++ app.py
@@ -1,7 +1,25 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/<nome>')
 def home(nome):
-    return f"<h1>Hello, {nome}!</h1>"
+    return render_template('home.html', nome = nome)
+
+@app.route('/login')
+def login():
+    return render_template('form.html')
+
+@app.route('/form', methods=['POST'])
+def form():
+    nome = request.form['nome']
+    senha = request.form['senha']
+
+    if senha == '123456':
+        mensagem = f'parabéns vc está logado {nome}!'
+
+    else:
+        mensagem = 'se logue direito rapais'
+
+    return render_template('answer.html', mensagem = mensagem)
+    
